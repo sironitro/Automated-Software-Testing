28603ca25dfc6dd9e7eab690ee1478cce822e34b
samyuktha-ramesh <samyuktha.ramesh@outlook.com>
2024-05-26 18:57:09 +0200

    changed number of runs

--- Test_Enviroment.py
+++ Test_Enviroment.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon May 20 10:06:19 2024
-
-@author: loris
-"""
-
 from Metamorphic_Testing.Linear_Programming import Linear_Programming_Testing
 from Metamorphic_Testing.Mixed_Integer_Linear_Programming import Mixed_Integer_Prgramming_Testing
 from Metamorphic_Testing.Quadratic_Programming import Quadratic_Programminng_Testing
 from Metamorphic_Testing.Assignment_Problem import Assignment_Problem_Testing
 from Metamorphic_Testing.Integer_Programming_Knapsack import Integer_Programming_Testing
-from Differential_Testing.Linear_Programming import test_prog
+from Fuzzing.LP_MIP import test_prog
 
 
 def Metamorphic_Test_Suite(filename):
@@ -30,8 +24,8 @@ def Differential_Test_Suite(filename):
         file.write("--------------------------------------------------------------\n") 
         file.write("Differential Testing:\n")
         file.write("--------------------------------------------------------------\n") 
-    test_prog(100, filename, mip=False) # test linear programming
-    test_prog(100, filename, mip=True) # test mixed-integer programming
+    test_prog(100000, filename, mip=False) # test linear programming
+    test_prog(100000, filename, mip=True) # test mixed-integer programming
     
-Metamorphic_Test_Suite("Metamorphic.txt")
-#@Differential_Test_Suite("Differential.txt")
+# Metamorphic_Test_Suite("Metamorphic.txt")
+Differential_Test_Suite("Fuzzing.txt")
